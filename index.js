0066f2a400b35713a20f384d0bdca8b64ac73197
Chase Wohrle <67084572+ChaseWohrle@users.noreply.github.com>
2020-10-07 13:47:32 -0700

    Update and rename script.js to index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function getDogImage() {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(responseJson => 
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results-img').replaceWith(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
