fe683af89fcef34170300991ce65b813ed1f3ec4
Chase Wohrle <chase.wohrle@gmail.com>
2020-10-11 16:28:13 -0700

    V2

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 'use strict';
 
-function getDogImage(inputNumber) {
-  console.log(inputNumber);
-  fetch(`https://dog.ceo/api/breeds/image/random/${inputNumber}`)
-    .then(response => response.json())
-    .then(responseJson => 
-      displayResults(responseJson))
-    .catch(error => alert(error));
+function getDogImage(inputBreed) {
+  console.log(inputBreed);
+  fetch(`https://dog.ceo/api/breed/${inputBreed}/images/random`).then(function(response){
+    if (response.ok) {
+      alert(response.ok)
+      response.json().then(function(responseJson) {
+        displayResults(responseJson);
+      })
+    } else{
+      alert("Breed Not Found");
+    }
+  })
+    
 }
+
 
 function displayResults(responseJson) {
   console.log(responseJson);
   let pictures = responseJson.message;
   let img_list = document.getElementById("img-list");
   img_list.innerHTML = '';
-  for (let i = 0; i < pictures.length; i++ ) {
-     var li = document.createElement('LI');
-     var img = document.createElement('IMG');
-     img.src = pictures[i]; 
-     li.appendChild(img);
-     img_list.append(li);
-  }
-
+  var li = document.createElement('LI');
+  var img = document.createElement('IMG');
+  img.src = pictures; 
+  li.appendChild(img);
+  img_list.append(li);
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const inputNumber = $('#input-number').val();
-    if (!inputNumber) {
-      getDogImage("3");
-    } else {
-      getDogImage(inputNumber);
-    }
+    const inputBreed = $('#input-breed').val();
+    getDogImage(inputBreed);
   });
 }
 
